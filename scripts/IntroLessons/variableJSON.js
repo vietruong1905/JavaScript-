0c072bb67a4a4f31f333c207729c8f38983fe36a
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-18 13:40:35 -0600

    update

---
+++ scripts/IntroLessons/variableJSON.js
@@ -0,0 +1,39 @@
+
+console.log("You made it to the console!");
+
+console.log('');
+console.log('');
+
+
+
+/*
+let person1 = {
+  firstName =" Viet" ;
+  lastName = "Truong";
+  age = " 17";
+  favoriteNumber = " 21";
+  hair ="black" ;
+  male = " true";
+  female=" fasle" ;
+  other =" fasle" ;
+}
+let person2 = {
+  firstName = " Luong";
+  lastName = " Ngoc" ;
+  age = "17";
+  favoriteNumber =" 89" ;
+  hair = "duty black hair";
+  male = "fasle";
+  female= " true";
+  other = " true";
+}
+let person3 = {
+  firstName = "Quang";
+  lastName ="Nguyen" ;
+  age ="17" ;
+  favoriteNumber =" 18" ;
+  hair = " duty black hair";
+  male =" true" ;
+  female="fasle";
+  other = "false";
+}
