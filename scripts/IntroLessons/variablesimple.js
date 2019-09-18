4cc157112a6fc72c3388a22a1b47bf01a78ca1f3
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-18 13:12:58 -0600

    update

---
+++ scripts/IntroLessons/variablesimple.js
@@ -0,0 +1,18 @@
+
+console.log("you made it to the console");
+
+console.log('');// Prints 2 spaces in the console
+console.log('');// Output easier to read
+
+console.log('Hello World');
+
+// Creat 10 Variables
+let firstName = " Viet";
+let lastName =" Truong";
+let age = 17;
+const favoriteNumber = 21;
+let hair = 'black';
+
+let male = true;
+let female = false;
+let other = false; 
