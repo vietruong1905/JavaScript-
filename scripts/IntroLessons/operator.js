76790a29a63590e2ed3d9641101f1cba796f67b0
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-19 13:37:18 -0600

    update

---
+++ scripts/IntroLessons/operator.js
@@ -0,0 +1,25 @@
+
+console.log("You made it to the console")
+
+function simpleOperators (numberA,numberB) {
+  let adding = numberA+numberB ;
+  let subtraction = numberA-numberB;
+  let mulitplying= umberA*numberB;
+  let division= numberA / numberB;
+  let modulus= numberA % numberB;
+  console.log(numberA, numberB, 'adding', adding);
+  console.log(numberA, numberB, 'subtraction', adding);
+  console.log(numberA, numberB, 'mulitplying', adding);
+  console.log(numberA, numberB, 'division', adding);
+  console.log(numberA, numberB, 'modulus', adding);
+
+
+
+
+}
+let number = [4,7,13,99,0,100,5,1,'1']
+
+
+ simpleOperators(number[0], number[1]);
+ simpleOperators(number[2], number[3]);
+ simpleOperators(number[4], number[5]);
