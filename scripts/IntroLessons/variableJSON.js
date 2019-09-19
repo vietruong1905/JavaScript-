5bf0302eb28252282de303da40e3d7b1099a167e
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-19 09:34:18 -0600

    update

--- scripts/IntroLessons/variableJSON.js
+++ scripts/IntroLessons/variableJSON.js
@@ -8,35 +8,34 @@ console.log('');
 
 /*
 let person1 = {
-  firstName =" Viet",
-  lastName = "Truong",
-  age = " 17";
-  favoriteNumber = " 21";
-  hair ="black" ;
-  male = " true";
-  female=" fasle" ;
-  other =" fasle" ;
+  firstName: =" Viet",
+  lastName: = "Truong",
+  age: = " 17",
+  favoriteNumber: = " 21",
+  hair: ="black",
+  mal: = " true",
+  female: =" fasle" ,
+  other :=" fasle" ,
 }
 let person2 = {
-  firstName = " Luong";
-  lastName = " Ngoc" ;
-  age = "17";
-  favoriteNumber =" 89" ;
-  hair = "duty black hair";
-  male = "fasle";
-  female= " true";
-  other = " true";
+  firstName: =" Luong",
+  lastName: = "Ngoc",
+  age: = " 17",
+  favoriteNumber: = " 78",
+  hair: ="black",
+  mal: = " true",
+  female: =" fasle" ,
+  other :=" fasle" ,
 }
-let person3 = {
-  firstName = "Quang";
-  lastName ="Nguyen" ;
-  age ="17" ;
-  favoriteNumber =" 18" ;
-  hair = " duty black hair";
-  male =" true" ;
-  female="fasle";
-  other = "false";
-}
+let person2 = {
+  firstName: =" Quang",
+  lastName: = "Nguyen",
+  age: = " 17",
+  favoriteNumber: = " 95",
+  hair: ="black",
+  mal: = " true",
+  female: =" fasle" ,
+  other :=" fasle" ,
 
  console.console.log(person1.LastName);
   console.console.log(person2.LastName);
