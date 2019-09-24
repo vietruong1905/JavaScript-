16d905c781692ef8be8deed1dcbc442646227400
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-24 13:46:02 -0600

    update

---
+++ scripts/IntroLessons/swappingVariables.js
@@ -0,0 +1,23 @@
+
+console.log("You made it to the console");
+
+let vietTruong = {
+  first: "viet",
+  last: "Truong",
+  age: 13,
+  haircolor: "black",
+}
+
+ let luongHoang = {
+   first: "Luong",
+   last: "Hoang",
+   age: 17,
+   haircolor:"black",
+ }
+
+ console.log(vietTruong);
+ console.log(luongHOang);
+  let swap= vietTruong[1];
+  vietTruong[1] =  luongHoang[1];
+  console.log(vietTruong);
+  console.log(luongHOang);
