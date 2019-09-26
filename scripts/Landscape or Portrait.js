ae9612696e4bf18c2e15d2a0e90a36fecebfcb70
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-26 13:42:29 -0600

    update

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -1,4 +1,4 @@
-console.log("You made cac");
+console.log("Luong nao lon");
 
 
 let stop = false; //Nota number test variabe;
@@ -18,15 +18,30 @@ function testNaN (number) {
 function main() {
  console.log("It works");
 
-let firstNumber, secondNumber, text;
+let firstNumber;
+let secondNumber;
+let text;
 
 // Get the width-value from id=textfield1
 firstNumber = document.getElementByID("textfield1").value;
 document.getElementByID("validityTest1").innerHTML = alert ( testNaN (firstNumber));
-
 document.getElementByID("validityTest1").innerHTML = testNaN (firstNumber);
 testNaN (firstNumber);
 
 // Get the Height-value from id=textfield2
 secondNumber = document.getElementByID("textfield2").value;
+
+document.getElementByID("validityTest1").innerHTML = alert ( testNaN (secondNumber));
+document.getElementByID("validityTest1").innerHTML = testNaN (secondtNumber);
+
+ if (stop == true) {
+   document.getElementByID("answer").innerHTML = "Restart the example, a width or height needs to be a number."
+ } else {
+    console.log("The display geometry is", geometry (firstNumber, secondNumber));
+    document.getElementByID('answer').innerHTML = "This display is " + geometry (firstNumber,secondNumber);
+ }
+
+
+
+
   }
