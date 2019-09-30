5b68ed997ca434a5183e3a022a240cda9f960bdb
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-09-30 13:55:40 -0600

    update

--- scripts/Landscape or Portrait.js
+++ scripts/Landscape or Portrait.js
@@ -4,7 +4,7 @@ console.log("Luong nao lon");
 let stop = false; //Nota number test variabe;
 
 function testNaN (number) {
-   console.log(stop);
+  // console.log(stop);
    if (isNaN (number) ) {
     stop = true;
     return "Type areal number"
@@ -12,8 +12,34 @@ function testNaN (number) {
   } else {
     return "Input Validated"
   }
-
 }
+ function geometry (width,height) {
+   if (width == height) return 'square'
+   return (width > height) ? 'Landscape' : 'Portrait' ;
+ }
+
+ function checkboxes() {
+  let checkbox1, checkbox2, checkbox3;
+  checkbox1 = document.getElementById('Checkbox1').checked;
+  checkbox2 = document.getElementById('Checkbox2').checked;
+  checkbox3 = document.getElementById('Checkbox3').checked;
+  if ( checkbox1 == true ) {
+    document.getElementById('checkbox1_answer').innerHTML = "Landscape";
+  } else {
+    document.getElementById('checkbox1_answer').innerHTML = "Answer?";
+  }
+  if ( checkbox2 == true ) {
+    document.getElementById('checkbox2_answer').innerHTML = "Portrait";
+  } else {
+    document.getElementById('checkbox2_answer').innerHTML = "Answer?";
+  }
+  if ( checkbox3 == true ) {
+    document.getElementById('checkbox3_answer').innerHTML = "Square";
+  } else {
+    document.getElementById('checkbox3_answer').innerHTML = "Answer?";
+  }
+}
+
 
 function main() {
  console.log("It works");
@@ -23,22 +49,22 @@ let secondNumber;
 let text;
 
 // Get the width-value from id=textfield1
-firstNumber = document.getElementByID("textfield1").value;
-document.getElementByID("validityTest1").innerHTML = alert ( testNaN (firstNumber));
-document.getElementByID("validityTest1").innerHTML = testNaN (firstNumber);
-testNaN (firstNumber);
+firstNumber = document.getElementById('textfield1').value;
+document.getElementById('validityTest1').innerHTML = alert ( testNaN (firstNumber));
+document.getElementById('validityTest1').innerHTML = testNaN (firstNumber);
+//testNaN (firstNumber);
 
 // Get the Height-value from id=textfield2
-secondNumber = document.getElementByID("textfield2").value;
+secondNumber = document.getElementById('textfield2').value;
 
-document.getElementByID("validityTest1").innerHTML = alert ( testNaN (secondNumber));
-document.getElementByID("validityTest1").innerHTML = testNaN (secondtNumber);
+document.getElementById('validityTest1').innerHTML = alert ( testNaN (secondNumber));
+document.getElementById('validityTest1').innerHTML = testNaN (secondNumber);
 
  if (stop == true) {
    document.getElementByID("answer").innerHTML = "Restart the example, a width or height needs to be a number."
  } else {
     console.log("The display geometry is", geometry (firstNumber, secondNumber));
-    document.getElementByID('answer').innerHTML = "This display is " + geometry (firstNumber,secondNumber);
+     document.getElementById("answer").innerHTML = "This display is " + geometry (firstNumber, secondNumber);
  }
 
 
