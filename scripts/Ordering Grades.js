88544ec307c5e37d5cc857a9a8fa70ec6d983d7b
vietruong1905 <54911695+vietruong1905@users.noreply.github.com>
2019-10-10 13:42:37 -0600

    update

--- scripts/Ordering Grades.js
+++ scripts/Ordering Grades.js
@@ -1,11 +1,9 @@
 //console.log("Did you change my file name?");
 console.log("You made it to the console!");
 
-let stop = false; //NAN Boolean Memory
-
+let stop = false;
 function testNaN (number) {
-  //console.log(stop);
-  if ( isNaN (number) ) {
+   if ( isNaN (number) ) {
     stop = true;
     return "Type a real number"
   } else {
@@ -13,13 +11,9 @@ function testNaN (number) {
   }
 }
 
-function orderingThree (first, second,third,fourth) { //Notice these parameters are local and do not mix with "number" variable
-  //Memory variable
+function orderingfour (first, second,third,fourth) {
   let memory1;
 
-  //If numbers out of order
-  //Use desending sequence to illustrate while loop ... if statements fail without WHILE since double switch required
-  //For example: 70, 60, 50 ,40
   while (first > second || second > third || third > fourth) {
     if (first > second) {
       memory1 = second;
@@ -37,14 +31,13 @@ function orderingThree (first, second,third,fourth) { //Notice these parameters 
       third = memory1;
     }
 
-  //Final Check: this checks if numbers are in order
-  //so checks if numbers are equal and catches if something is wrong
+
   if (first <= second) {
     if (second <= third) {
       if (third <= fourth) {
-      return first + ", " second + ", " third + "," fourth;
-    }}}
-  } else {
+      return first + ", " + second + ", " + third + "," + fourth;
+    }}}}
+   else {{{{
     return "Check program for errors"
   }
 }
@@ -77,41 +70,41 @@ function checkboxes() {
   }
 }
 
-// This coding illustrates the advanced method
+
 function main() {
-  //console.log("It works");
+
 
   let firstNumber, secondNumber, thirdNumber; fourthNumber;
 
-  //Get the Width-value from id=textfield1
+
   firstNumber = document.getElementById('textfield1').value;
-  //testNaN (firstNumber);
+
   document.getElementById('validityTest1').innerHTML = alert ( testNaN (firstNumber));
   document.getElementById('validityTest1').innerHTML = testNaN (firstNumber);
 
-  //Get the Height-value from id=textfield2
+
   secondNumber = document.getElementById('textfield2').value;
-  //testNaN (secondNumber);
+
   document.getElementById('validityTest2').innerHTML = alert ( testNaN (secondNumber));
   document.getElementById('validityTest2').innerHTML = testNaN (secondNumber);
 
-  //Get the Height-value from id=textfield2
+
   thirdNumber = document.getElementById('textfield3').value;
-  //testNaN (secondNumber);
+
   document.getElementById('validityTest3').innerHTML = alert ( testNaN (thirdNumber));
   document.getElementById('validityTest3').innerHTML = testNaN (thirdNumber);
 
   fourthNumber = document.getElementById('textfield4').value;
-  //testNaN (secondNumber);
+
   document.getElementById('validityTest4').innerHTML = alert ( testNaN (fourthNumber));
   document.getElementById('validityTest4').innerHTML = testNaN (fourthNumber);
 
 
 
-  //Illustrates one example of character escapes
+
   console.log("In memory, \n the first number is " + firstNumber + " and \n the second number is " + secondNumber + " and \n the third number is " + thirdNumber + " and \n the fourth number is " + fourthNumber);
 
-  //Final Output to inner HTML & Console
+
 
   if (stop == true) {
     document.getElementById('answer').innerHTML = "Restart the example, a width or height needs to be a number."
