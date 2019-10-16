c3ea5903b0e95d6c532178466da469fe6d561925
Viet Truong <v.truong6@EDU.epsb.ca>
2019-10-16 13:51:45 -0600

    update

---
+++ scripts/Modulus Assignment.js
@@ -0,0 +1,16 @@
+console.log("Luong nao lon");
+
+
+let stop = false; //Nota number test variabe;
+
+function testNaN (number) {
+   console.log(stop);
+   if (isNaN (number) ) {
+    stop = true;
+    return "Type areal number"
+
+  } else {
+    return "Input Validated"
+  }
+}
+function 
